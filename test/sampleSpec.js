631a181c04364f5b5ee26c53c63d9a34da380194
Ethan <emekanwankwo201@gmail.com>
2017-11-16 12:41:31 +0100

    add mocha/chai test environment

---
+++ test/sampleSpec.js
@@ -0,0 +1,7 @@
+let expect = require('chai').expect;
+
+describe('Sample test script', () => {
+    it('Should return true for dummy test case', () => {
+        expect(1 + 1).to.equal(2);
+    });
+});
